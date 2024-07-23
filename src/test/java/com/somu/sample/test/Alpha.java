db26bb16c2c238a7ac20f37407b877864e0755a2
SomasundaramRajendran <somasundaram.r98@gmail.com>
2024-07-23 22:37:52 +0530

    adding my code

---
+++ src/test/java/com/somu/sample/test/Alpha.java
@@ -0,0 +1,20 @@
+package com.somu.sample.test;
+
+public class Alpha {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+			for(int i=6;i>=1;i--)
+			{
+				for(int j=1;j<=i;j++)
+				{
+					
+					System.out.print((char) (j+64) +" ");
+				}
+				System.out.println();
+			}
+	}
+
+}
